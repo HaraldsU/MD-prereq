79cd850b3adb5e0bc9d1d7bc4e6eb7dd71653478
HaraldsU <upitisharalds@gmail.com>
2026-04-14 02:32:13 +0300

    Added openai API calls. Added new model for semantic evaluation.

--- scripts/IIR/IIR_CE.py
+++ scripts/IIR/IIR_CE.py
@@ -1,9 +1,10 @@
 from dotenv import load_dotenv
 from anthropic import Anthropic
+from openai import OpenAI
 from pathlib import Path
 from datetime import datetime
 from nltk.stem import SnowballStemmer
-from sentence_transformers import SentenceTransformer, util
+from sentence_transformers import SentenceTransformer
 import json
 import csv
 import re
@@ -11,6 +12,7 @@ import ast
 import string
 import simplemma
 import torch
+import os
 
 def call_antrophic_api(section_name: str, section_text: str) -> str:
 # {{{
@@ -42,21 +44,43 @@ def call_antrophic_api(section_name: str, section_text: str) -> str:
     # print(response.input_tokens)
 # }}}
 
+def call_openai_api():
+# {{{
+    api_url = os.getenv("API_URL_GEMMA")
+    print(api_url)
+    model = "google/gemma-3-27b-it"
+    client = OpenAI(
+        base_url = api_url,
+        api_key = "any_key"
+    )
+    response = client.chat.completions.create(
+        messages = [{"role": "user", "content": "HELLO, TESTING."}],
+        max_tokens = 20,
+        temperature = 0.2,
+        model = model
+    )
+    print(response.choices[0].message.content)
+# }}}
+
 def load_and_predict_IIR():
 # {{{
     print('--- Getting predictions ---')
 
     # section_number, section_name, section_text, source_url
-    with open(IIR_sections_json_path, 'r') as f:
-        IIR_sections_obj = json.load(f)
+    with open(IIR_SECTIONS_JSON_PATH, 'r') as f:
+        iir_sections_obj = json.load(f)
 
     predictions = []
-    IIR_gt_paths = get_IIR_ground_truth_paths()
+    iir_gt_paths = get_IIR_ground_truth_paths()
+    print(iir_sections_obj)
+    print(iir_gt_paths)
 
-    for section, gt_path in zip(IIR_sections_obj, IIR_gt_paths):
+    for section, gt_path in zip(iir_sections_obj, iir_gt_paths):
         section_number = section['section_number']
+        print('section_number = ', section_number)
         
         if section_number in SECTIONS or SECTIONS == [0]:
+        # {{{
             section_name = section['section_name']
             section_text = section['section_text']
             print(section_number, section_name)
@@ -88,6 +112,7 @@ def load_and_predict_IIR():
                 'gtc_count_to_text_len_ratio (%)': round((len(ground_truth) / len(section_text)) * 100, 3),
                 'section_text_count': len(section_text)
             })
+        # }}}
 
     write_json(predictions, 'predictions')
 # }}}
@@ -104,7 +129,6 @@ def evaluate_IIR():
     predictions_filename = PATH_PREFIX + 'predictions.json'
     predictions_json_path = Path('~/Downloads/prereq/scripts/IIR/' + str(predictions_filename)).expanduser()
 
-    # 'section_number', 'section_name', 'predicted_concepts (pc)', 'pc_count', 'pc_count_to_text_len_ratio (%)', 'ground_truth_concepts (gtc)', 'gtc_count', 'gtc_count_to_text_len_ratio (%)', 'section_text_count'
     with open(predictions_json_path, 'r') as o:
         predictions_obj = json.load(o)
 
@@ -115,17 +139,16 @@ def evaluate_IIR():
     rec_total = 0.0
     f1_total = 0.0
 
-    prec_total_sent = 0.0
-    rec_total_sent = 0.0
-    f1_total_sent = 0.0
+    sent_sem_total = 0.0
 
-    prec_total_sim = 0.0
-    rec_total_sim = 0.0
-    f1_total_sim = 0.0
+    prec_total_sem = 0.0
+    rec_total_sem = 0.0
+    f1_total_sem = 0.0
 
     for prediction in predictions_obj:
     # {{{
         section_number = prediction['section_number']
+        # print('section_number = ', section_number)
 
         if section_number in SECTIONS or SECTIONS == [0]:
             predictions = prediction['predicted_concepts (pc)']
@@ -137,20 +160,18 @@ def evaluate_IIR():
             prec, rec, f1 = calc_statistical_metrics(predictions, ground_truth)
 
             print(f'{prediction['section_number']} {prediction['section_name']}: p={prec:.3f} r={rec:.3f} f1={f1:.3f}')
-            prec_sent, rec_sent, f1_sent, prec_sim, rec_sim, f1_sim = calc_semantical_metrics(predictions, ground_truth)
+            sent_sem, prec_sem, rec_sem, f1_sem = calc_semantical_metrics(predictions, ground_truth)
             print()
 
             prec_total += prec
             rec_total += rec
             f1_total += f1
 
-            prec_total_sent += prec_sent
-            rec_total_sent += rec_sent
-            f1_total_sent += f1_sent
+            sent_sem_total += sent_sem 
 
-            prec_total_sim += prec_sim
-            rec_total_sim += rec_sim
-            f1_total_sim += f1_sim
+            prec_total_sem += prec_sem
+            rec_total_sem += rec_sem
+            f1_total_sem += f1_sem
 
             count += 1
 
@@ -161,13 +182,11 @@ def evaluate_IIR():
                     'recall': round(rec, 3),
                     'f1': round(f1, 3),
 
-                    'precision_sentence': round(prec_sent, 3),
-                    'recall_sentence': round(rec_sent, 3),
-                    'f1_sentence': round(f1_sent, 3),
+                    'sentence_sem': round(sent_sem, 3),
 
-                    'precision_sim': round(prec_sim, 3),
-                    'recall_sim': round(rec_sim, 3),
-                    'f1_sim': round(f1_sim, 3),
+                    'precision_sem': round(prec_sem, 3),
+                    'recall_sem': round(rec_sem, 3),
+                    'f1_sem': round(f1_sem, 3),
 
                     'pc_count': prediction['pc_count'],
                     'pc_count_to_text_len_ratio (%)': prediction['pc_count_to_text_len_ratio (%)'],
@@ -184,18 +203,16 @@ def evaluate_IIR():
             'recall': round(rec_total / count, 3),
             'f1': round(f1_total / count, 3),
 
-            'precision_sentence': round(prec_total_sent / count, 3),
-            'recall_sentence': round(rec_total_sent / count, 3),
-            'f1_sentence': round(f1_total_sent / count, 3),
+            'sentence_sem': round(sent_sem_total / count, 3),
 
-            'precision_sim': round(prec_total_sim / count, 3),
-            'recall_sim': round(rec_total_sim / count, 3),
-            'f1_sim': round(f1_total_sim / count, 3)
+            'precision_sem': round(prec_total_sem / count, 3),
+            'recall_sem': round(rec_total_sem / count, 3),
+            'f1_sem': round(f1_total_sem / count, 3)
         })
 
     print(f'Average: prec_total={(prec_total / count):.3f} rec_total={(rec_total / count):.3f} f1_total:{(f1_total / count):.3f}')
-    print(f'Average (sentence): prec_total_sent={(prec_total_sent / count):.3f} rec_total_sent={(rec_total_sent / count):.3f} f1_total_sent:{(f1_total_sent / count):.3f}')
-    print(f'Average (sim): prec_total_sim={(prec_total_sim / count):.3f} rec_total_sim={(rec_total_sim / count):.3f} f1_total_sim:{(f1_total_sim / count):.3f}')
+    print(f'Average (sentence)={(sent_sem_total / count):.3f}')
+    print(f'Average (sem): prec_total_sem={(prec_total_sem / count):.3f} rec_total_sem={(rec_total_sem / count):.3f} f1_total_sem:{(f1_total_sem / count):.3f}')
     write_json(evaluation, 'evaluation')
 # }}}
 
@@ -203,18 +220,19 @@ def get_IIR_ground_truth_paths() -> list[str]:
 # {{{
     # iir-{section_number}.csv
     pattern = r'iir-(\d+(?:\.\d+)*)\.csv'
-    IIR_csvs = []
+    iir_csvs = []
     
-    for file in IIR_folder_path.iterdir():
+    for file in IIR_FOLDER_PATH.iterdir():
         if file.is_file():
             reg = re.match(pattern, file.name)
 
             if reg:
+                # if reg.group(1) <= int(SECTIONS[0]) or SECTIONS == [0]:
                 if reg.group(1) in SECTIONS or SECTIONS == [0]:
-                    IIR_csvs.append(str(file))
+                    iir_csvs.append(str(file))
 
-    IIR_csvs.sort(key=lambda f: tuple(int(x) for x in re.match(pattern, Path(f).name).group(1).split('.')))
-    return IIR_csvs
+    iir_csvs.sort(key=lambda f: tuple(int(x) for x in re.match(pattern, Path(f).name).group(1).split('.')))
+    return iir_csvs
 # }}}
 
 def normalize_words(words: list[str]) -> list[str]:
@@ -225,11 +243,10 @@ def normalize_words(words: list[str]) -> list[str]:
         cleaned = ' '.join(re.sub(f'[{re.escape(string.punctuation)}]', ' ', word).lower().split())
         # lemmatized = ' '.join(simplemma.lemmatize(w, lang='en') for w in cleaned.split())
         # result.append(lemmatized)
-
         stemmed = ' '.join(stemmer.stem(w) for w in cleaned.split())
         result.append(stemmed)
 
-    return result
+    return list(dict.fromkeys(result))
 # }}}
 
 def calc_statistical_metrics(predicted: list[str], ground_truth: list[str]) -> tuple[float, float, float]:
@@ -257,41 +274,37 @@ def calc_semantical_metrics(predicted: list[str], ground_truth: list[str]):
 
     emb_pred_sent = SENTENCE_MODEL.encode(predicted_concat, normalize_embeddings = True, compression_ratio = 0.5)
     emb_gt_sent = SENTENCE_MODEL.encode(ground_truth_concat, normalize_embeddings = True, compression_ratio = 0.5)
+    # emb_pred_sent = SENTENCE_MODEL.encode(predicted_concat, normalize_embeddings = True)
+    # emb_gt_sent = SENTENCE_MODEL.encode(ground_truth_concat, normalize_embeddings = True)
 
-    sim_sent = SENTENCE_MODEL.similarity(emb_pred_sent, emb_gt_sent)
-    prec_sent = sim_sent.max(axis=1).values.mean().item()
-    rec_sent = sim_sent.max(axis=0).values.mean().item()
-    f1_sent = 2 * prec_sent * rec_sent / (prec_sent + rec_sent)
-    print(f'Sentence similarity: p={prec_sent:.3f}  r={rec_sent:.3f}  f1={f1_sent:.3f}')
+    sent_sem = SENTENCE_MODEL.similarity(emb_pred_sent, emb_gt_sent).item()
+    print(f'Sentence similarity={sent_sem:.3f}')
 
-    emb_pred_sim = SENTENCE_MODEL.encode(predicted, normalize_embeddings = True, compression_ratio = 0.5)
-    emb_gt_sim = SENTENCE_MODEL.encode(ground_truth, normalize_embeddings = True, compression_ratio = 0.5)
+    # emb_pred_sem = SENTENCE_MODEL.encode(predicted, normalize_embeddings = True, compression_ratio = 0.5)
+    # emb_gt_sem = SENTENCE_MODEL.encode(ground_truth, normalize_embeddings = True, compression_ratio = 0.5)
+    emb_pred_sem = SIM_MODEL.encode(predicted)
+    emb_gt_sem = SIM_MODEL.encode(ground_truth)
 
-    # print('emb_pred.shape = ', emb_pred.shape)
-    # print('emb_gt.shape = ', emb_gt.shape)
-    # print('emb_pred = ', emb_pred)
-    # print('emb_gt = ', emb_gt)
-    
-    sim_sim = SENTENCE_MODEL.similarity(emb_pred_sim, emb_gt_sim)
-    prec_sim = sim_sim.max(axis=1).values.mean().item()
-    rec_sim = sim_sim.max(axis=0).values.mean().item()
-    f1_sim = 2 * prec_sim * rec_sim / (prec_sim + rec_sim)
-    print(f'Greedy list similarity: p={prec_sim:.3f}  r={rec_sim:.3f}  f1={f1_sim:.3f}')
+    sem = SIM_MODEL.similarity(emb_pred_sem, emb_gt_sem)
+    prec_sem = sem.amax(dim = 1).mean().item()
+    rec_sem = sem.amax(dim = 0).mean().item()
+    f1_sem = 2 * prec_sem * rec_sem / (prec_sem + rec_sem)
+    print(f'Greedy list similarity: p={prec_sem:.3f}  r={rec_sem:.3f}  f1={f1_sem:.3f}')
 
-    return prec_sent, rec_sent, f1_sent, prec_sim, rec_sim, f1_sim
+    return sent_sem, prec_sem, rec_sem, f1_sem
 # }}}
 
 # --- Paths ---
-IIR_sections_json_path = Path('~/Downloads/prereq/scripts/IIR/iir_sections.json').expanduser()
-IIR_folder_path = Path('~/Downloads/prereq/datasets/IIR-dataset/annotation').expanduser()
+IIR_SECTIONS_JSON_PATH = Path('~/Downloads/prereq/scripts/IIR/iir_sections.json').expanduser()
+IIR_FOLDER_PATH = Path('~/Downloads/prereq/datasets/IIR-dataset/annotation').expanduser()
 
 # --- Models ---
 # MODEL = 'claude-opus-4-6'
 # MODEL = 'claude-sonnet-4-6'
 MODEL = 'claude-haiku-4-5'
 MAX_TOKENS = '1024'
-# MAX_TOKENS = 2048
-# MAX_TOKENS = 4096
+# MAX_TOKENS = '2048'
+# MAX_TOKENS = '4096'
 
 # --- Methods ---
 METHOD = 'ONE-SHOT'
@@ -301,14 +314,16 @@ TIMESTAMP = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 # TIMESTAMP = '2026-04-11_04-31-20' # For testing
 
 # --- Sections ---
-SECTIONS = ['1']
-# SECTIONS = ['1', '1.1', '1.2', '1.3', '1.4']
+# SECTIONS = ['1']
+SECTIONS = ['1', '1.1', '1.2', '1.3', '1.4']
+# SECTIONS = ['4.4']
 # SECTIONS = [0] # All sections
 
 # --- Other ---
 # NORMALIZATION = 'LEMMATIZATION'
 NORMALIZATION = 'STEMMED'
-PATH_PREFIX = MODEL + '_' + MAX_TOKENS + '_' + METHOD + '_' + NORMALIZATION + '_' + TIMESTAMP + '_'
+stemmer = SnowballStemmer('english')
+
 SENTENCE_MODEL = SentenceTransformer(
     'infgrad/Jasper-Token-Compression-600M',
     processor_kwargs={
@@ -319,7 +334,13 @@ SENTENCE_MODEL = SentenceTransformer(
     trust_remote_code=True,
     device='cuda',
 )
-stemmer = SnowballStemmer('english')
+# SENTENCE_MODEL = SentenceTransformer('sentence-transformers/all-MiniLM-L12-v2')
+# SENTENCE_MODEL = SentenceTransformer('sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2')
+# SENTENCE_MODEL = SentenceTransformer('sentence-transformers/LaBSE')
+
+SIM_MODEL = SentenceTransformer('uclanlp/keyphrase-mpnet-v1')
+
+PATH_PREFIX = MODEL + '_' + MAX_TOKENS + '_' + METHOD + '_' + NORMALIZATION + '_' + TIMESTAMP + '_'
 
 print('TIMESTAMP = ', TIMESTAMP)
 print('MODEL = ', MODEL)
@@ -329,9 +350,7 @@ print('NORMALIZATION = ', NORMALIZATION)
 print()
 
 load_dotenv()
-load_and_predict_IIR() # Makes API calls
-evaluate_IIR()
-# calc_semantical_metrics(['How do I tie a shoelace?'], ['The GDP of Japan in 1987'])
-# calc_semantical_metrics(['7x^2 + 3x - 5 = 0'], ['The dog barked loudly at the mailman'])
-# calc_semantical_metrics(['Hello'], ['Hello'])
+# load_and_predict_IIR() # Makes API calls
+# evaluate_IIR()
+call_openai_api()
 
